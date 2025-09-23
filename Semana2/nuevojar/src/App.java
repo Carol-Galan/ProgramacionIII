45fe3fd962c07c504e52ba05b3037f5f74d4e6f6
alumno <alumno@usal.es>
2025-09-23 19:52:25 +0200

    ejercicio1

--- Semana2/nuevojar/src/App.java
+++ Semana2/nuevojar/src/App.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Introduzca el año de nacimiento:");
+        int anioNac = scan.nextInt();
+        System.out.println("Introduzca el año actual:");
+        int anioAct = scan.nextInt();
+
+        System.out.printf("Su edad es: %d", anioAct-anioNac);
+
+        scan.close();
+
+
+
+
     }
 }
