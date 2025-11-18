2990665cc360444c6c42b5e86bcdbb2282faa3ce
alumno <alumno@usal.es>
2025-11-18 20:01:33 +0100

    ejercicio 5

---
+++ Semana10/Ejercicio5/src/Documento.java
@@ -0,0 +1,10 @@
+public class Documento extends Archivo implements Imprimible{
+    public Documento (String nombre, int tamanio){
+        super(nombre, tamanio);
+    }
+
+    @Override
+    public void imprimir(){
+        
+    }
+}
---
+++ Semana10/Ejercicio5/src/Imagen.java
@@ -0,0 +1,5 @@
+public class Imagen extends Archivo implements Compartible{
+    public Imagen(String nombre, int tamanio){
+        super(nombre, tamanio);
+    }
+}
